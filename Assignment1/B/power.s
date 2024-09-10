ecd9af2fe1090d228cfc25397e371ab7235c220a
Simeon Vutov <simeon32.dev@gmail.com>
2024-09-10 15:36:56 +0200

    Added comments to the power.s file

--- Assignment1/B/power.s
+++ Assignment1/B/power.s
@@ -1,72 +1,76 @@
 .data
-base: .long 0               # base variable
-exp:  .long 0
+base: .long 0               # base variable size(32 bits)
+exp:  .long 0               # exp - exponent variable size(32 bits)
 
-.text
-basePrompt: .asciz "Enter a non negative base number: "
-expPrompt: .asciz "Enter a non negative exponent number: "
-resultPrompt: .asciz "The result is: %ld\n"
-format: .asciz "%d"
+.text                       
+basePrompt: .asciz "Enter a non negative base number: "            # base prompt text constant for printf
+expPrompt: .asciz "Enter a non negative exponent number: "         # exp prompt text constant for printf 
+resultPrompt: .asciz "The result is: %ld\n"                        # result prompt text constant for printf
+format: .asciz "%d"                                                # format string for getting long number from scanf
 
 .global main
 
 main:
-    push %rbp               # prologue
-    mov %rsp, %rbp
+                            # PROLOGUE
+    push %rbp               # push the base pointer
+    mov %rsp, %rbp          # copy stack pointer value to base pointer
 
-    mov $0, %rax            #no vector
-    mov $basePrompt, %rdi
-    call printf
+    mov $0, %rax            # no vector registers in use for printf
+    mov $basePrompt, %rdi   # first parameter: basePrompt string
+    call printf             # call printf to print the string
 
-    mov $0, %rax
-    mov $format, %rdi
-    mov $base, %rsi
-    call scanf
+    mov $0, %rax            # no vector registers in use for scanf
+    mov $format, %rdi       # first parameter: input format string
+    mov $base, %rsi         # second parameter: address where to save input from scanf
+    call scanf              # call scanf to get the base value
 
-    mov $0, %rax
-    mov $expPrompt, %rdi
-    call printf
+    mov $0, %rax            # no vector registers in use for printf 
+    mov $expPrompt, %rdi    # first parameter: expPrompt string
+    call printf             # call printf to print the string
 
-    mov $0, %rax
-    mov $format, %rdi
-    mov $exp, %rsi
-    call scanf
+    mov $0, %rax            # no vector registers in use for scanf
+    mov $format, %rdi       # first parameter: input format string
+    mov $exp, %rsi          # second parameter: address where to save input from scanf
+    call scanf              # call scanf to get the base value
     
-    call pow
+    call pow                # call pow subroutine           
 
-    mov $resultPrompt, %rdi
-    mov %rax, %rsi
-    mov $0, %rax
-    call printf
+    mov $resultPrompt, %rdi     # first parameter: resultPrompt string
+    mov %rax, %rsi              # second paramter: copy the result value after the calculations from rax register
+    mov $0, %rax                # no vector registers in use for scanf
+    call printf                 # call printf to print the result
 
-    mov %rbp, %rsp          # epilogue
-    pop %rbp
+                            # EPILOGUE
+    mov %rbp, %rsp          # clear local variables from the stack
+    pop %rbp                # restore base pointer location
 
-    mov $0, %rdi
-    call exit
+    mov $0, %rdi            # setting zero status code as first parameter for exit
+    call exit               # call exit to stop execution
 
-pow:
-    push %rbp               # prologue
-    mov %rsp, %rbp
+pow:                        # subroutine for calculating the result of param1 to the power of param2
+                            # PROLOGUE
+    push %rbp               # push the base pointer
+    mov %rsp, %rbp          # copy stack pointer value to base pointer
 
-    sub $8, %rsp            # Reserve space on the stack for local variable: total
-    movq $1, %rsp       # Setting local variable total to 1
+    sub $8, %rsp            # reserve space on the stack for local variable: total(64 bits)
+    movq $1, %rsp           # setting local variable total to 1
 
-    condition:
-        cmpl $0, exp
-        jg loop
+    condition:              # checks if exp > 0
+        cmpl $0, exp        # compare EXP to 0
+        jg loop             # jump to loop if greater than 0 
     
-
-    mov %rbp, %rsp          # epilogue
-    pop %rbp
+                            # EPILOGUE
+    mov %rbp, %rsp          # clear local variables from the stack
+    pop %rbp                # restore base pointer location
     
-    ret
+    ret                     # exit pow subroutine
 
     loop:
-        # total = total * base
-        mov %rsp, %rax
-        mull base
-        mov %rax, %rsp  # Update total to the result of total * %rax
-        subl $1, exp
-        jmp condition
+                            # total = total * base
+        movq %rsp, %rax     # copy the address of local variable total to RAX
+        mull base           # multiply RAX with base value and result is stored again in RAX
+        
+        mov %rax, %rsp      # storing the value of total in RSP(location of local variable total)
+        subl $1, exp        # decrementing exponent after the multiplication
+        jmp condition       # jump to condition
 
