d47dff862e4141816f300c1e6684467b89dcdf39
Simeon Vutov <simeon32.dev@gmail.com>
2024-10-06 20:15:25 +0200

    Added implementation for decoder to solve Assignment4. There is some kind of proble when using final.s where no colors are used. Also added other example files(abc_sorted, final)

--- Assignment4/decoder.s
+++ Assignment4/decoder.s
@@ -1,8 +1,46 @@
 .text
 format: .asciz "%d"
 formatChar: .asciz "%c"
+colorEffectFormat:  .asciz "\033[%d;%dm"
+
+# Special Effects' codes
+resetEffects: .asciz "\033[0m"
+stopBlinkingEffect: .asciz "\033[25m"
+boldEffect: .asciz "\033[1m"
+faintEffect: .asciz "\033[2m"
+concealEffect: .asciz "\033[8m"
+revealEffect: .asciz "\033[28m"
+blinkEffect: .asciz "\033[5m"
 
 .include "final.s"
+
+foregroundColors:
+    .quad 30  # Black
+    .quad 31  # Red
+    .quad 32  # Green
+    .quad 33  # Yellow
+    .quad 34  # Blue
+    .quad 35  # Magenta
+    .quad 36  # Cyan
+    .quad 37  # White
+backgroundColors:
+    .quad 40  # Black
+    .quad 41  # Red
+    .quad 42  # Green
+    .quad 43  # Yellow
+    .quad 44  # Blue
+    .quad 45  # Magenta
+    .quad 46  # Cyan
+    .quad 47  # White
+    
+specialEffects:
+    .quad 0, resetEffects      # Reset to normal
+    .quad 37, stopBlinkingEffect   # Stop blinking
+    .quad 42, boldEffect           # Bold
+    .quad 66, faintEffect          # Faint
+    .quad 105, concealEffect       # Conceal
+    .quad 153, revealEffect        # Reveal
+    .quad 182, blinkEffect          # Blink
 
 .global main
 
@@ -33,9 +71,15 @@ decode:
     movq %r12, %r13         # set current element to be the first one
 
     loop1:
-        # leaq +1(%r13), %rsi     # get number of times to print the letter
+        movzbq 6(%r13), %rdi    # get foreground color
+        movzbq 7(%r13), %rsi    # get background color
+        
+    # Set background and foreground
+        call set_effects
+        
         movzbq (%r13), %rdi     # get actual character byte and extend it to quad
-        movzbq 1(%r13), %rsi     # get number of times to print and extend it to quad
+        movzbq 1(%r13), %rsi    # get number of times to print and extend it to quad
+
         call print              # first param - character, second param - amount
         
         movl 2(%r13), %eax       # Store index value(4 bytes) in eax
@@ -75,7 +119,7 @@ print:
 
     movq %rdi, %r14         # Save first parameter in r14 to free rdi
     movq %rsi, %r15         # Save second parameter in r15 to free rsi
-
+    
     loop:
         cmpq $0, %r15       # compare printTimes with 0
         jle endPrint        # if printTimes is less or equal to 0 we stop printing
@@ -96,6 +140,52 @@ print:
         movq %rbp, %rsp     # clear local variables from stack
         popq %rbp           # restore base pointer
         ret                 # return from print Subroutine
+
+set_effects:
+    pushq %rbp
+    movq %rsp, %rbp
+    
+    # Calculate the address for background color
+    cmpq %rdi, %rsi
+    jne colorEffects
+    
+    otherEffects:
+        movq $0, %rcx               # Set loop counter to 0
+    
+        loop_effects:
+            movq %rcx, %rax                 # Copy value of counter to rax
+            shlq $4, %rax                   # shift left 4 times rax (counter * 16)
+            addq $specialEffects, %rax      # add start address of specialEffects table
+            movq (%rax), %r8
+
+            cmpq %rdi, %r8                 # compare foreground number to 
+            je applyEffect
+            
+            addq $1, %rcx
+            cmpq $7, %rcx
+            jl loop_effects
+    
+        jmp endSetEffects
+
+    applyEffect:
+            addq $8, %rax
+            movq (%rax), %rdi
+            call printf
+            jmp endSetEffects
+    
+    colorEffects:
+        shlq $3, %rdi
+        shlq $3, %rsi
+        movq backgroundColors(%rsi), %rdx
+        movq foregroundColors(%rdi), %rsi
+        movq $colorEffectFormat, %rdi   # Load format string into %rdi
+        call printf
+
+    endSetEffects:
+        # Restore stack frame
+        movq %rbp, %rsp
+        popq %rbp
+        ret
 
 main:
 	pushq	%rbp 			# push the base pointer (and align the stack)
