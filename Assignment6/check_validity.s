68fa9b79ef433406dc943756e3b3bd8410940fd9
Shureto <georgishureto123@gmail.com>
2024-10-09 00:38:37 +0200

    final touches

--- Assignment6/check_validity.s
+++ Assignment6/check_validity.s
@@ -1,10 +1,6 @@
-.text
-valid: .string "valid"
-invalid: .string "invalid"
-
-.data              # Data section for initialized variables
-valid_msg: .ascii "valid\0"      # Define a null-terminated string for "valid"
-invalid_msg: .ascii "invalid\0"  # Define a null-terminated string for "invalid"
+.text             
+valid_msg: .asciz "valid"      # Define a null-terminated string for "valid"
+invalid_msg: .asciz "invalid"  # Define a null-terminated string for "invalid"
 
 .include "basic.s"
 
@@ -22,23 +18,25 @@ check_validity:
 	pushq	%rbp 			# push the base pointer (and align the stack)
 	movq	%rsp, %rbp		# copy stack pointer value to base pointer
 
+	movq $1, %rsi			# boolean variable whether parantheses are valid
+	movq $0, %rdx			# amount of open brackets that are not "closed"
+	
 	movq $0, %r11			# counter (which char are we at, "i" in for loop)
 
-		iterating_string:
-		movb    (%rdi, %r11), %al  # Load the current character into %al
-    	testb   %al, %al           # Check if it's the null terminator
-    	je      end_count          # If yes, jump to end_count
+		iterating_string:   # loop
 
-			check_whether_count_of_open_brackets_is_zero:
-			cmpq $0, %rdx
-			je reset_steps_back
+		movb    (%rdi, %r11), %al  # load the current character into %al - rdi(starting address), r11 - counter
+    	cmpb    $0, %al            # check if it's the null terminator
+    	je      end_count          # if yes, jump to end_count
 
-			base_false_check_if_first_bracket_is_closed:
+			check_1:
+			# check for closed bracket after open brackets becomes zero
+
 			cmpq $0, %rdx  						# check if open bracktes are zero
-			jne check_if_next_bracket_is_open   # skip checks if not equal
+			jne check_if_next_bracket_is_open   # skip below checks, if not equal to 0
 
-			# check for closed bracket, and go to invalid if so
-			cmpb $')', %al		
+			# check for closed bracket, and go to invalid, if so
+			cmpb $')', %al 	
 			je set_invalid
 			cmpb $']', %al
 			je set_invalid
@@ -49,90 +47,89 @@ check_validity:
 
 			check_if_next_bracket_is_open:
 			cmpb $'(', %al
-			je increment_open_bracket_counter_and_back_to_loop
+			je push_open_bracket
 			cmpb $'[', %al
-			je increment_open_bracket_counter_and_back_to_loop
+			je push_open_bracket
 			cmpb $'{', %al
-			je increment_open_bracket_counter_and_back_to_loop
+			je push_open_bracket
 			cmpb $'<', %al
-			je increment_open_bracket_counter_and_back_to_loop
+			je push_open_bracket
 
-			check_if_current_closed_bracket_corresponds_to_an_open:
-			# calculating the open bracket (tracking back)
-			# r11(current) - rcx(steps back)
-			# r10 - calculated offset
-			movq %r11, %r10
-			subq %rcx, %r10
-			movb    (%rdi, %r10), %r8b  # Load the current character into %r11b
+			check_if_brackets_match:
+			
+				check_bracket_2_0:
+				cmpb $')', %al			# check if current closing bracket is ')'
+				jne check_bracket_2_1	# if not, go check for next type of parenthesis
+				addq $8, %rsp			# prepare stack for pop (aligning stack)
+				popq %r8				# finally we pop the opening bracket
+				cmpb $'(', %r8b  		# check if calculated open bracket is the same as closed
+				je update_after_brackets_match
+				jmp set_invalid			# if closed bracket doesn't correspond to open
 
-			check_bracket_2_0:
-			cmpb $')', %al
-			jne check_bracket_2_1
-			cmpb $'(', %r8b  		# check if calculated open bracket is the same as closed
-			je closed_bracket_corresponds_to_open_so_we_update
+				check_bracket_2_1:
+				cmpb $']', %al			# check if current closing bracket is ']'
+				jne check_bracket_2_2	# if not, go check for next type of parenthesis
+				addq $8, %rsp			# prepare stack for pop (aligning stack)
+				popq %r8           		# finally we pop the opening bracket
+				cmpb $'[', %r8b			# check if calculated open bracket is the same as closed
+				je update_after_brackets_match
+				jmp set_invalid   		# if closed bracket doesn't correspond to open
 
-			check_bracket_2_1:
-			cmpb $']', %al
-			jne check_bracket_2_2
-			cmpb $'[', %r8b		# check if calculated open bracket is the same as closed
-			je closed_bracket_corresponds_to_open_so_we_update
 
-			check_bracket_2_2:
-			cmpb $'}', %al
-			jne check_bracket_2_3
-			cmpb $'{', %r8b		# check if calculated open bracket is the same as closed
-			je closed_bracket_corresponds_to_open_so_we_update
+				check_bracket_2_2:
+				cmpb $'}', %al			# check if current closing bracket is '}'
+				jne check_bracket_2_3	# if not, go check for next type of parenthesis
+				addq $8, %rsp			# prepare stack for pop (aligning stack)
+				popq %r8				# finally we pop the opening bracket
+				cmpb $'{', %r8b			# check if calculated open bracket is the same as closed
+				je update_after_brackets_match
+				jmp set_invalid   		# if closed bracket doesn't correspond to open
 
-			check_bracket_2_3:
-			cmpb $'>', %al
-			cmpb $'<', %r8b		# check if calculated open bracket is the same as closed
-			je closed_bracket_corresponds_to_open_so_we_update
 
-			# iff closed bracket doesn't correspond to open
-			jmp set_invalid
+				check_bracket_2_3:
+				cmpb $'>', %al			# check if current closing bracket is '>'
+				addq $8, %rsp			# prepare stack for pop (aligning stack)
+				popq %r8				# finally we pop the opening bracket
+				cmpb $'<', %r8b			# check if calculated open bracket is the same as closed
+				je update_after_brackets_match
+				jmp set_invalid   		# iff closed bracket doesn't correspond to open
 
-			increment_open_bracket_counter_and_back_to_loop:
-			incq %rdx 		# open bracket counter
-			jmp next_cycle
+			push_open_bracket:
+			movzbq %al, %rax    # zero-extend %al to %rax (64-bit)
+			pushq %rax          # push the 64-bit %rax onto the stack
+			subq $8, %rsp 		# aligning stack
+			incq %rdx			# incrementing open bracket counter
+			jmp next_cycle      # continue iterating
 
-			closed_bracket_corresponds_to_open_so_we_update:
-			decq %rdx		# open bracket counter
-			addq $2, %rcx	# steps back
-			jmp next_cycle
+			update_after_brackets_match:
+			decq %rdx			# open bracket counter decremented by one, because we "closed"
+			jmp next_cycle		# continue iterating
 
 		next_cycle:
-    	incq    %r11               # Increment the character count
-    	jmp     iterating_string         # Repeat the loop
+    	incq    %r11               		 # increment the character counter
+    	jmp     iterating_string         # repeat the loop
 		
 		set_invalid:
 		# break, and set to invalid
-			movq $0, %rsi
-			jmp end_count
-
-			reset_steps_back:
-				movq $1, %rcx
-				jmp base_false_check_if_first_bracket_is_closed
+			movq $0, %rsi 			# rsi - false
+			jmp end_count			# break from loop
 
 	end_count:
+	# check if there are left open brackets
 
-	# check if there are left open brackets	
 	cmpq $0, %rdx
-	jne print_invalid
+	jne return_invalid
 
-	# print based on value in rsi
+	cmpq $0, %rsi				# rsi - boolean validator
+	je return_invalid
 
-	cmpq $0, %rsi
-	je print_invalid
-
-	# print valid
-	movq $valid_msg, %rax
+	movq $valid_msg, %rax		# valid
 	jmp epilogue_
 
-	print_invalid:
-	movq $invalid_msg, %rax
+	return_invalid:
+	movq $invalid_msg, %rax		# invalid
 
 	epilogue_:
-	# epilogue
 	movq	%rbp, %rsp		# clear local variables from stack
 	popq	%rbp			# restore base pointer location 
 	ret
@@ -141,18 +138,8 @@ main:
 	pushq	%rbp 			# push the base pointer (and align the stack)
 	movq	%rsp, %rbp		# copy stack pointer value to base pointer
 
-#	movq $1, %rax			# boolean (0 - false, 1 - true) validator
-#	movq $0, %r8			# count of open brackets
-#	movq $1, %r9			# number of stepsToGoBackToCheckForMatching
-
 	movq	$MESSAGE, %rdi		# first parameter: address of the message
-	movq $1, %rsi
-	movq $0, %rdx
-	movq $1, %rcx
 	call	check_validity		# call check_validity
-
-	movq %rax, %rdi
-	call printf
 
 	popq	%rbp			# restore base pointer location 
 	movq	$0, %rdi		# load program exit code
