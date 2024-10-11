dac43b7a5bea3c5a236233a69e82b78f6cbe1a3a
Simeon Vutov <simeon32.dev@gmail.com>
2024-10-11 23:47:52 +0200

    Fixed all of the bugs in the code and finilized the implementation. Added some test code

--- Assignment10/brainfuck.s
+++ Assignment10/brainfuck.s
@@ -6,8 +6,6 @@ memory_cells: .skip 30000
 
 .text
 format_str: .asciz "We should be executing the following code:\n%s"
-input_format_str: .asciz "%c"
-output_format_str: .asciz "%d"
 
 .global brainfuck
 
@@ -50,10 +48,10 @@ brainfuck:
             je read
             
             cmpb $'>', %al 
-            je moveLeftPointer
+            je moveRightPointer
             
             cmpb $'<', %al
-            je moveRightPointer
+            je moveLeftPointer
         
         loop_check:
             cmpb $'[', %al
@@ -65,40 +63,41 @@ brainfuck:
             jmp loop
         
         increaseMemoryCell:
-            leaq memory_cells(,%r14,8), %rax 
-            incq (%rax)
+            leaq memory_cells(,%r14), %rax 
+            incb (%rax)
             jmp next
+
         decreaseMemoryCell:
-            leaq memory_cells(,%r14, 8), %rax
-            decq (%rax)
+            leaq memory_cells(,%r14), %rax
+            decb (%rax)
+            jmp next
+
+        print:
+            leaq memory_cells(,%r14), %rdi
+            movb (%rdi), %dil
+            call putchar
+            jmp next
+
+        read:
+            call getchar
+            leaq memory_cells(,%r14), %rsi
+            movb %al, (%rsi)
             jmp next
         moveLeftPointer:
-            cmpq $1, %r14
+            cmpq $0, %r14
             je end_brainfuck
             decq %r14
             jmp next
         moveRightPointer:
             cmpq $29999, %r14
-            je  end_brainfuck
+            je end_brainfuck
             incq %r14
-            jmp next
-        print:
-            leaq memory_cells(,%r14,8), %rax
-            movq $output_format_str, %rdi
-            movzbq (%rax), %rsi
-            movq $0, %rax # no-vector arguments
-            call printf
-            jmp next
-        read:
-            leaq memory_cells(,%r14, 8), %rsi
-            movq $input_format_str, %rdi
-            call scanf
             jmp next
         handle_start_loop:
             cmpb $1, skip_loop                 # if skip loop is true we do not execute regularly
             je skip_start_loop_case
-            leaq memory_cells(, %r14,8), %rax
-            cmpq $0, (%rax)
+            leaq memory_cells(, %r14), %rax
+            cmpb $0, (%rax)
             je skip_loop_logic
             pushq %r13                          # save address of beginning of the loop
             subq $8, %rsp                       # align stack
@@ -110,9 +109,9 @@ brainfuck:
                 jmp next
             
             skip_loop_logic:
-                movq $1, skip_loop
+                movb $1, skip_loop
                 incq %rbx
-            jmp next 
+                jmp next 
         handle_end_loop:
             cmpb $1, skip_loop
             jne continue_end_loop
@@ -121,20 +120,19 @@ brainfuck:
             je reset_skip_loop
             jmp next
         continue_end_loop:
-            leaq memory_cells(, %r14, 8), %rax
-            cmpq $0, (%rax)
+            leaq memory_cells(, %r14), %rax
+            cmpb $0, (%rax)
             jne return_to_start_of_loop
             addq $16, %rsp                      # remove the bracket address because we need to exit the loop
             decq %r15
             jmp next
-            #PROBLEM
         return_to_start_of_loop:
             addq $8, %rsp
             popq %r13                            # pop the address of start of loop
             jmp loop
         reset_skip_loop:
             movq $0, %rbx
-            movq $0, skip_loop
+            movb $0, skip_loop
             jmp next
     end_brainfuck:
         movq %rbp, %rsp
